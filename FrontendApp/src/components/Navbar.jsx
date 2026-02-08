399761f9a43dcd21ad1a7f2677db03ccaf2c5d9a
Kushal J <jkushal208@gmail.com>
2026-02-08 17:43:14 +0530

    add authetication and via clerk

--- FrontendApp/src/components/Navbar.jsx
+++ FrontendApp/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { assets } from './../assets/assets'
-import { MenuIcon, Search, SearchIcon, XCircleIcon, XIcon } from 'lucide-react'
+import { Film, MenuIcon, Search, SearchIcon, Ticket, XCircleIcon, XIcon } from 'lucide-react'
+import { SignInButton, useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
@@ -25,6 +26,15 @@ const Navbar = () => {
   ]
 
 
+  const [isnavopen, setisnavopen] = useState(false)
+
+  const {user}=useUser()
+  const {openSignIn}=useClerk()
+
+
+  const navigate=useNavigate()
+
+
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
 
@@ -34,30 +44,41 @@ const Navbar = () => {
 
 
 
-      <div className='max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
-                      z-50 flex flex-col md:flex-row items-center
-                      max-md:text-lg
+      <div className={`${isnavopen ? "max-md:w-full":"max-md:w-0"} max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
+                      z-50 flex flex-col md:flex-row items-center max-md:text-lg
                       max-md:justify-center gap-8 md:px-8 py-3 max-md:h-screen
                       md:rounded-full backdrop-blur bg-b1ack/70 md:bg-white/10 md:border
-                      border-gray-300/20 overflow-hidden transition- [width] duration-300' >
+                      border-gray-300/20 overflow-hidden transition-[width] duration-300`} >
 
-        <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' />
-        {navLinks.map((link) => (
+        <XIcon onClick={()=>setisnavopen(false)} className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' />
+        {navLinks.map((link,idx) => (
 
-          <Link to={link.Path}>{link.Title}</Link>
+          <Link key={idx} to={link.Path}>{link.Title}</Link>
 
         ))}
 
       </div>
 
-      <div className='flex items-center gap-6 '>
+      <div className='flex items-center gap-8 '>
 
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-l sm:px-7 sm:py-2 bg-primary
-                          hover:bg-primary-dull transition rounded-full font-medium
-                          cursor-pointer' >Login</button>
+       
+        {!user ? (
+          <button onClick={()=>openSignIn()} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
+                            hover:bg-primary-dull transition rounded-full font-medium
+                            cursor-pointer' >Login</button>
+          
+
+        ):(
+          <UserButton>
+
+            <UserButton.MenuItems>
+              <UserButton.Action label='My bookings'  labelIcon={<Ticket width={20}/>} onClick={()=>navigate('/mybookings')}/>
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
+      <MenuIcon onClick={()=>setisnavopen(!isnavopen)} className='max-md:m1-4 md:hidden w-8 h-8 cursor-pointer' />
       </div>
-      <MenuIcon className='max-md:m1-4 md:hidden w-8 h-8 cursor-pointer' />
 
     </div>
 
