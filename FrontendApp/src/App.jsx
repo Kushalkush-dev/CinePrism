8bdad13a356c917adc47d86bd49cd00321b96728
Kushal J <jkushal208@gmail.com>
2026-02-11 20:38:35 +0530

    render movie cards in movies page

--- FrontendApp/src/App.jsx
+++ FrontendApp/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Movies from './components/Movies'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Movies from './pages/Movies'
 
 
 const App = () => {
--- FrontendApp/src/components/Movies.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Movies = () => {
-  return (
-    <div className='text-5xl'>Movies</div>
-  )
-}
-
-export default Movies
