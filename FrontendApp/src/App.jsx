8fe8ea6bc82add6c09589f3b87caef942bd48292
Kushal J <jkushal208@gmail.com>
2026-02-11 20:51:47 +0530

    add favorites page

--- FrontendApp/src/App.jsx
+++ FrontendApp/src/App.jsx
@@ -4,21 +4,24 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Movies from './pages/Movies'
+import Favorites from './pages/Favorites'
 
 
 const App = () => {
   return (
     <>
-  <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/movies' element={<Movies/>}/>
-      <Route path='/movies/:id'/>
-      
-      <Route path='/*' element={<><h1>404 Go back page unavailable</h1></>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/favorites' element={<Favorites />} />
 
-    <Footer/>
+        <Route path='/movies/:id' />
+
+        <Route path='/*' element={<><h1>404 Go back page unavailable</h1></>} />
+      </Routes>
+
+      <Footer />
     </>
 
   )
---
+++ FrontendApp/src/pages/Favorites.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard'
+import BackgroundCircle from '../components/BackgroundCircle'
+const Favorites = () => {
+
+  return dummyShowsData.length > 0 ? (
+
+    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+
+      <BackgroundCircle top='150px' left='0px' />
+      <BackgroundCircle top='150px' left='0px' />
+      <BackgroundCircle top='600px' right='0px' />
+      <BackgroundCircle top='600px' right='0px' />
+      <h1 className='sm:text-2xl text-xl font-medium my-4'>Your Favorite Movies</h1>
+
+      <div className='flex flex-wrap max-sm:justify-center gap-8'>
+        {dummyShowsData.map((movie) => (
+
+          <MovieCard movie={movie} key={movie._id} />
+
+        ))}
+      </div>
+    </div>
+
+  ) :
+    (
+
+      <div className='flex h-screen justify-center items-center'>
+        <p className='sm:text-xl text-sm  '>No Movies Available Currently come back later! </p>
+      </div>
+    )
+}
+
+export default Favorites
